9c8c200f001f55fb5c38ca8a146d20e9f8c6ab6e
herdeybayor <shereefdeen4real@gmail.com>
2023-05-21 16:51:56 +0100

    feat: scaffold

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * _putchar - puts a character to the standard output
+ *
+ * @c: character argument
+ * Return: char c and return -1 if error
+ */
+
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+int main()
+{
+    _putchar('Z');
+    _putchar('\n');
+
+    return (0);
+}
